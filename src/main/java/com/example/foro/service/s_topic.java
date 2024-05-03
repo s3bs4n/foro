9b4278538d088ceb07a96f5df085ad00e7ad9b64
s3b_s4n <SEB.SANTIBANEZG@DUOCUC.CL>
2024-05-02 22:59:30 -0400

    Antes de crear la carpeta Service

--- src/main/java/com/example/foro/service/s_topic.java
+++ src/main/java/com/example/foro/service/s_topic.java
@@ -1,5 +1,14 @@
 package com.example.foro.service;
 
+import com.example.foro.model.c_topic;
+import java.util.List;
+import java.util.Optional;
+
+
 public class s_topic {
-    
+    List<Student> getAllStudents();
+    Optional<Student> getStudentById(Long id);
+    Student createStudent(Student student);
+    Student updateStudent(Long id,Student student);
+    void deleteStudent(Long id);
 }
